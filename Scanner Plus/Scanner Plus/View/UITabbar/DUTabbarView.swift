d7a83023d9f03cfab45cc65be3c7f77882a9d8b1
tuanhust <tuanhust2010@gmail.com>
2017-12-11 17:37:08 +0700

    push code

---
+++ Scanner Plus/Scanner Plus/View/UITabbar/DUTabbarView.swift
@@ -0,0 +1,44 @@
+//
+//  DUTabbar.swift
+//  Scanner Plus
+//
+//  Created by TuanNM on 12/11/17.
+//  Copyright © 2017 TuanNM. All rights reserved.
+//
+
+import UIKit
+
+class DUTabbarView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    fileprivate func setupView() {
+        let view = viewFromNibForClass()
+        view.frame = bounds
+        view.autoresizingMask = [
+            UIViewAutoresizing.flexibleWidth,
+            UIViewAutoresizing.flexibleHeight
+        ]
+        addSubview(view)
+        
+        
+    }
+    
+    // Loads a XIB file into a view and returns this view.
+    fileprivate func viewFromNibForClass() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        return view
+    }
+}
+
+
